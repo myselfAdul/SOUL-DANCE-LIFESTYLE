582637aba35d2098e0297e242b5a09b14a3815f3
myselfadul <myselfadul@gmail.com>
2024-08-23 03:04:30 +0600

    anchor tag @media fix

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,9 +7,9 @@ const Navbar = () => {
 
   return (
     <div className=" flex items-center justify-between py-5 font-medium">
-      <NavLink to="/">
+      <Link to="/">
         <img src={assets.logo} className="w-40" alt="" />
-      </NavLink>
+      </Link>
 
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
         <NavLink to="/" className="flex flex-col items-center gap-1 ">
